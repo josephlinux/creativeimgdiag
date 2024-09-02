b1927f6e7b3a3b5e82749452b2cd6df16497825a
Joseph Madanu <15406547+josephlinux@users.noreply.github.com>
2024-09-02 16:36:39 +0530

    Delete imgdiag directory

--- imgdiag/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
- $host="localhost";
- $user="root";
- $password="";
-$link=mysql_connect($host,$user,$password) or die ("could not connect to the mysql".mysql_error());
-mysql_select_db("imgdiag");
-
-?>
-
--- imgdiag/footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Design and Developed by Creative Billing Software Team";
-?>
